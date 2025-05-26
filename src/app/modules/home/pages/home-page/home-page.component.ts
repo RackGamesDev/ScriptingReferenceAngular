816138958784d19721cfa842e51c9d5c92acc2e2
RackGamesDev <yesterdiscord7@gmail.com>
2025-05-26 16:51:37 +0200

    Avances

--- src/app/modules/home/pages/home-page/home-page.component.ts
+++ src/app/modules/home/pages/home-page/home-page.component.ts
@@ -41,16 +41,30 @@ export class HomePageComponent implements OnInit, OnDestroy {
     AutentificacionService.incrementarNumero(); //Llamando a una funcion de un servicio
     console.log(AutentificacionService.getNumero());
 
-    const observador:Subscription = this.autentificacionService.callback.subscribe((response: string) => { //Subscibiendose a un evento en un servicio, la funcion se ejecutara cada vez que se emita desde cualquier lado
+    const observador: Subscription = this.autentificacionService.callback.subscribe((response: string) => { //Subscibiendose a un evento en un servicio, la funcion se ejecutara cada vez que se emita desde cualquier lado
       console.log("Se emitio al evento con " + response);
     });
     this.autentificacionService.callback.emit("hola"); //Emitiendo al evento, por lo que se ejecutaran las funciones donde se haya suscrito a este
     observador.unsubscribe(); //Desuscribiendose del evento, para que no se ejecute mas (se suele hace en ngOnDestroy para que no se quede consumiendo)
-  }
+    const observadorTextos: Subscription = this.autentificacionService.textos.subscribe((response: string[]) => { //Suscribiendose a un observable, una variable que emite informacion
+      console.log("es " + response);
+    });
+    observadorTextos.unsubscribe();
 
+    const suscripcionesFuncion = async () => { //En algunos casos ciertas suscripciones deben ser asincronas
+      const observadorNumeros: Subscription = this.autentificacionService.numerosAleatorios.subscribe({next:(response: number[]) => { //Suscribiendose a un observable con funcion
+        response.forEach((numero: number) => console.log(numero));
+      }, complete: () => {console.log("fin de emisiones")}, error: (e: any) => {console.warn(e)}}); //Ademas, estableciendo eventos cuando deje de emitir finalmente o cuando de error
+      
+      observadorNumeros.unsubscribe();
+    }
+    //suscripcionesFuncion();
+  }
   ngOnDestroy(): void { //Cuando se destruya el componente (cuando se navega a otra ruta o se elimina)
     console.log("componente destruido");
   } //Tambien estan ngOnChanges, ngDoCheck, ngAfterContentInit, ngAfterContentChecked, ngAfterViewInit, ngAfterViewChecked
+
+
 
   funcion(event: Event): void { //Esta funcion se puede llamar desde el html
     console.log("funcion llamada " + event); //Event tiene informaicon del evento que llamo desde el html
--- src/app/shared/services/autentificacion.service.ts
+++ src/app/shared/services/autentificacion.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, EventEmitter } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -22,5 +23,16 @@ export class AutentificacionService {
 
   callback:EventEmitter<any> = new EventEmitter<any>(); //Creando un evento en el servicio al que se pueden suscribir componentes y pueden enviar datos (hace de intermediario)
 
-  constructor() { }
+  textos:Observable<string[]> = of([]); //Un observable es una variable que emite valores, para leerla hay que suscribirse
+  numerosAleatorios:Observable<number[]> = new Observable<number[]>((observer) => { //Similar al anterior pero se emiten con una funcion asincronamente
+    setInterval(() => {
+      observer.next([Math.random(), Math.random(), Math.random()]);//Emitiendo un array para ser escuchado pos los suscriptores, puede tardar un tiempo
+      if (Math.random() > 0.7) observer.complete(); //Cerrando finalmente, indicando que ya no se emitira nada mas 
+      if (Math.random() > 0.9) observer.error("Error de emision"); //Emitiendo un error, que se puede capturar en el suscriptor
+    }, 2000);
+  });
+
+  constructor() { 
+    this.textos = of(['texto1', 'texto2', 'texto3']); //El servicio se encarga de alimentar a la variable
+  }
 }
